a585f8fe0c32e5eba1e3e24d9402d0546e575ebc
FuryNashwaAspirna <FurryAspirnaa11@gmail.com>
2020-02-04 08:25:28 +0700

    tugas kel12

--- resources/views/olshop.blade.php
+++ resources/views/olshop.blade.php
@@ -19,7 +19,7 @@
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <img src="{{'img/olshop.jpg'}}" width="100px">
+    <img src="{{'img/olshop1.jpg'}}" width="100px">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
