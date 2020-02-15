b35b3eac0f805393002f484fdf9f22bce9c7c86d
FuryNashwaAspirna <FurryAspirnaa11@gmail.com>
2020-02-15 10:49:04 +0700

    laravelku

--- resources/views/olshop.blade.php
+++ resources/views/olshop.blade.php
@@ -7,6 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/style.css">
 
     <title>Hello, world!</title>
     
@@ -14,16 +15,31 @@
     .col-2{
         text
     }
+    .container{
+      background-color: #5b8c85;
+    }
+  .row{
+    background-color: #5b8c85;
+  }
+  .carousel-inner{
+    width:100%;
+    max-height: 500px !important;
+  }
+  
     </style>
 
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <img src="{{'img/olshop1.jpg'}}" width="100px">
+    <nav class="navbar navbar-expand-lg navbar-light bg-#5b8c85" style="background-color: #ffffff;">
+    <img src="img/UNIQLO1.png" width="7%" height="7%">
+    <div class="collape navbar-collapse" id="navbarText">
+
+    </div>
+      
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-      
+    
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
@@ -40,44 +56,206 @@
             </li>
           </ul>
           <form class="form-inline my-2 my-lg-0">
-            <a href="#" class="btn btn-primary mx-md-2">Register</a>
+            <a href="#" class="btn btn-danger mx-md-2">Register</a>
             <span class="mx-md-3">/</span>
-            <a href="#" class="btn btn-primary mx-md-3">Login</a>
+            <a href="#" class="btn btn-danger mx-md-3">Login</a>
           </form>
         </div>
       </nav>
       <div>
-          <div class="container">
+         
               <div class="row">
                   
-                  <div class="col-2 mx-auto btn mx-md-3" style="color:red;">Menu</div>
-                  <div class="col-2 mx-auto btn mx-md-4" style="color:red;">Menu</div>
-                  <div class="col-2 mx-auto btn mx-md-5" style="color:red;">Menu</div>
-                  <div class="col-2 mx-auto btn mx-md-3" style="color:red;">Menu</div>
+                  <div class="col-2 mx-auto btn mx-md-3" style="color:red; font-size:20px;">HOME</div>
+                  <div class="col-2 mx-auto btn mx-md-4" style="color:red; font-size:20px;">PRODUK</div>
+                  <div class="col-2 mx-auto btn mx-md-5" style="color:red; font-size:20px;">CONTACT US</div>
+                  <div class="col-2 mx-auto btn mx-md-3" style="color:red; font-size:20px;">ABOUT US</div>
               </div>
-          </div>
+      
 
-        <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
             <div class="carousel-inner">
-              <div class="carousel-item active" data-interval="10000">
-                <img src="img/123.jpg" class="d-block w-100" alt="..." height="400px" width="400px" >
-              </div>
-              <div class="carousel-item" data-interval="2000">
-                <img src="img/tas2.jpg" class="d-block w-100" alt="..." height="400px" width="400px">
+              <div class="carousel-item active">
+                <img src="img/slider1.jpg" height="500px" class="d-block w-100" alt="...">
               </div>
               <div class="carousel-item">
-                <img src="img/baju1.jpg" class="d-block w-100" alt="..." height="400px" width="400px">
+                <img src="img/slider2.jpg" height="500px" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="img/slider3.jpg" height="500px"class="d-block w-100" alt="...">
               </div>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
               <span class="sr-only">Previous</span>
             </a>
-            <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
               <span class="carousel-control-next-icon" aria-hidden="true"></span>
               <span class="sr-only">Next</span>
             </a>
           </div>
+          
+          <div class="container">
+            <h1 class="text-center mt-5">Produk Terbaru</h1>
+            <div class="row justify-content-center ">
+              <div class="card mt-2 mb-2 ml-2" style="width: 16rem;">
+                <img src="img/jaket1.jpg" height="200px" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">SWEAT HOODIE </h5>
+                  <p class="card-text">Size: All Size</p>
+                  <p class="card-text">Condition: Good</p>
+                  <p class="card-text">Harga: Rp.200.000</p>
+                  <a href="#" class="btn btn-primary">Add</a>
+                  <a href="#" class="btn btn-secondary">Detail</a>
+                </div>
+              </div>
+              <div class="card mt-2 mb-2 ml-2" style="width: 16rem;">
+                <img src="img/uniglobrownblouse.jpg" height="200px" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">UNIQLO Brown Blouse</h5>
+                  <p class="card-text">Size: M</p>
+                  <p class="card-text">Condition: Good</p>
+                  <p>Harga: Rp.150.000</p>
+                  <a href="#" class="btn btn-primary">Add</a>
+                  <a href="#" class="btn btn-secondary">Detail</a>
+                </div>
+              </div>
+              <div class="card mt-2 mb-2 ml-2" style="width: 16rem;">
+                <img src="img/UT.png" height="200px" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">UNIQLO UTS</h5>
+                  <p class="card-text">Size: L & XL</p>
+                  <p class="card-text">Condition: Good</p>
+                  <p>Harga: Rp.145.000</p>
+                  <a href="#" class="btn btn-primary">Add</a>
+                  <a href="#" class="btn btn-secondary">Detail</a>
+                </div>
+              </div>
+              <div class="card mt-2 mb-2 ml-2" style="width: 16rem;">
+                <img src="img/baju4.jpg" height="200px" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Kemeja Blus</h5>
+                  <p class="card-text">Size: M & L</p>
+                  <p class="card-text">Condition: Good</p>
+                  <p class="card-text">Harga: Rp.200.000</p>
+                  <a href="#" class="btn btn-primary">Add</a>
+                  <a href="#" class="btn btn-secondary">Detail</a>
+                </div>
+              </div>
+        
+            </div>
+          </div>
+          <div class="container">
+            <h1 class="text-center mt-5" style="color: red;">Discounted Products</h1>
+            <div class="row justify-content-center ">
+              <div class="card mt-2 mb-2 ml-2" style="width: 16rem;">
+                <img src="img/celana.jpg" height="200px" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">UNIQLO CELANA CHINO SLIM FIT</h5>
+                  <p class="card-text">Size: ALL SIZE</p>
+                  <p class="card-text">Condition: Good</p>
+                  <p class="card-text">DISCOUNT 30%</p>
+                  <a href="#" class="btn btn-primary">Add</a>
+                  <a href="#" class="btn btn-secondary">Detail</a>
+                </div>
+              </div>
+              <div class="card mt-2 mb-2 ml-2" style="width: 16rem;">
+                <img src="img/ROK.jpg" height="200px" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">UNIQLO PEACH PLEATED SKIRT</h5>
+                  <p class="card-text">Size: ALL SIZE</p>
+                  <p class="card-text">Condition: Good</p>
+                  <p class="card-text">DISCOUNT 20%</p>
+                  <a href="#" class="btn btn-primary">Add</a>
+                  <a href="#" class="btn btn-secondary">Detail</a>
+                </div>
+              </div>
+              <div class="card mt-2 mb-2 ml-2" style="width: 16rem;">
+                <img src="img/celana2.jpg" height="200px" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">UNIQLO JOGER</h5>
+                  <p class="card-text">Size: M & L</p>
+                  <p class="card-text">Condition: Good</p>
+                  <p class="card-text">DISCOUNT 50%</p>
+                  <a href="#" class="btn btn-primary">Add</a>
+                  <a href="#" class="btn btn-secondary">Detail</a>
+                </div>
+              </div>
+              <div class="card mt-2 mb-2 ml-2" style="width: 16rem;">
+                <img src="img/CELANA3.jpg" height="200px" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">UNIQLO dry easy </h5>
+                  <p class="card-text">Size: S & L</p>
+                  <p class="card-text">Condition: Good</p>
+                  <p class="card-text">DISCOUNT 20%</p>
+                  <a href="#" class="btn btn-primary">Add</a>
+                  <a href="#" class="btn btn-secondary">Detail</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="container">
+            <h1 class="text-center mt-5">Produk Terlaris</h1>
+            <div class="row justify-content-center ">
+          <div class="card mt-2 mb-2 ml-2" style="width: 16rem;">
+            <img src="img/BLACK.jpg" height="200px" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">BLACK GARDIGAN</h5>
+              <p class="card-text">Size: M & L</p>
+              <p class="card-text">Condition: Good</p>
+              <p class="card-text">Harga: Rp.200.000</p>
+              <a href="#" class="btn btn-primary">Add</a>
+              <a href="#" class="btn btn-secondary">Detail</a>
+            </div>
+          </div>
+          <div class="card mt-2 mb-2 ml-2" style="width: 16rem;">
+            <img src="img/UTS.png" height="200px" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Kemeja Blus</h5>
+              <p class="card-text">Size: M & L</p>
+              <p class="card-text">Condition: Good</p>
+              <p class="card-text">Harga: Rp.200.000</p>
+              <a href="#" class="btn btn-primary">Add</a>
+              <a href="#" class="btn btn-secondary">Detail</a>
+            </div>
+          </div>
+          <div class="card mt-2 mb-2 ml-2" style="width: 16rem;">
+            <img src="img/bajubayi.jpg" height="200px" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">UNIQLO BABY</h5>
+              <p class="card-text">Size: ALL SIZE</p>
+              <p class="card-text">Condition: Good</p>
+              <p class="card-text">Harga: Rp.230.000</p>
+              <a href="#" class="btn btn-primary">Add</a>
+              <a href="#" class="btn btn-secondary">Detail</a>
+            </div>
+          </div>
+          <div class="card mt-2 mb-2 ml-2" style="width: 16rem;">
+            <img src="img/baby.jpg" height="200px" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">UNIQLO BABY TIGER</h5>
+              <p class="card-text">Size: ALL SIZE</p>
+              <p class="card-text">Condition: Good</p>
+              <p class="card-text"> Harga : Rp. 200.000</p>
+              <a href="#" class="btn btn-primary">Add</a>
+              <a href="#" class="btn btn-secondary">Detail</a>
+            </div>
+          </div>
+            </div>
+          </div>
+         
+          
+        <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://www.uniqlo.com/id/CPaGoods/itemcode=413432/education/bootstrap/"> MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+    
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
